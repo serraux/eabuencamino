b8cffc62e0e9b3fbfb7686f284beac9bc4ba606b
serraux <augux607@gmail.com>
2025-05-23 08:42:07 -0600

    adicin de pag navegacin

--- js/main.js
+++ js/main.js
@@ -117,3 +117,21 @@ document.addEventListener('DOMContentLoaded', () => {
   // Mostrar la imagen inicial
   showImage(current);
 
+/**Animacion imagen en seccion about us */ 
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.fade-on-scroll');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove('opacity-0', 'translate-y-4');
+          entry.target.classList.add('opacity-100', 'translate-y-0');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.2
+    });
+
+    items.forEach(item => observer.observe(item));
+  });
